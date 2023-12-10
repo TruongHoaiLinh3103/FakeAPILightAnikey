8b7bb2b5b0ea372d3cdb9acbe0f8021af1c8cc40
Anikey <eagleremlinh@gmail.com>
2023-12-11 02:06:23 +0700

    Ok

--- api/server.js
+++ api/server.js
@@ -6,14 +6,10 @@ const middlewares = jsonServer.defaults()
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
 // You can use the one used by JSON Server
-server.use(
-  jsonServer.rewriter({
-    "/*": "$1",
-  })
-)
+server.use(jsonServer.bodyParser)
 // Use default router
 server.use(router)
 server.listen(8000, () => {
   console.log('JSON Server is running')
 })
-module.exports = server
+module.exports = server;
