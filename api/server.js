ea21889a7babc6fed53d06b881a1b32faed7ca38
Anikey <eagleremlinh@gmail.com>
2023-12-11 02:01:23 +0700

    Ok

--- api/server.js
+++ api/server.js
@@ -5,18 +5,15 @@ const middlewares = jsonServer.defaults()
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
-
-// Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
-
-// To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
-
+server.use(
+  jsonServer.rewriter({
+    "/*": "$1",
+  })
+)
 // Use default router
 server.use(router)
 server.listen(8000, () => {
   console.log('JSON Server is running')
 })
+module.exports = server
