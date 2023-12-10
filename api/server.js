cb9dfb3d79d6a94b1d454ba65cd6d89714a5e5d2
Anikey <eagleremlinh@gmail.com>
2023-12-11 00:23:49 +0700

    A

--- api/server.js
+++ api/server.js
@@ -1,31 +1,29 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-
 const server = jsonServer.create()
-
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
 const router = jsonServer.router('db.json')
-
 const middlewares = jsonServer.defaults()
 
+// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
 })
 
-// Export the Server API
-module.exports = server
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// Use default router
+server.use(router)
+server.listen(8000, () => {
+  console.log('JSON Server is running')
+})
