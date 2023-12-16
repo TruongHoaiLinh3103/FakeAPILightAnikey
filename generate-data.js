69d3c5953ce5fab65422f8fab75f5de56573b24d
Anikey <eagleremlinh@gmail.com>
2023-12-16 13:16:43 +0700

    LiZong

--- generate-data.js
+++ generate-data.js
@@ -2,35 +2,73 @@ casual = require('casual');
 const { faker } = require('@faker-js/faker');
 const fs = require("fs");
 
-const randomcommentList = (n) => {
+
+//COMMENT
+// const randomcommentList = (n) => {
+//     if(n < 0){
+//         return [];
+//     }
+//     const commentList = [];
+//     Array.from(new Array(n)).forEach((eliment, index) => {
+//         const comment = {
+//             id: index,
+//             name: casual.name,
+//             avatar: faker.image.avatar(),
+//             like: Math.floor(Math.random() * 100),
+//             unchat: Math.floor(Math.random() * 10),
+//             unlike: Math.floor(Math.random() * 100),
+//             comment: casual.description,
+//             productId: Math.floor(Math.random() * 500),
+//             createAt: Date.now(),
+//             updateAt: Date.now()
+//         }
+//         commentList.push(comment);
+//     })
+//     return commentList;
+// }
+
+// (() => {
+//     //Total 100
+//     const commentList = randomcommentList(10000);
+//     // prepare db object
+//     const backup = {
+//         comment: commentList
+//     };
+//     // write db object to db.json
+//     fs.writeFile("backup.json", JSON.stringify(backup), () => {
+//         console.log("Ok");
+//     });
+// })();
+
+
+//PRODUCT
+const randomproductList = (n) => {
     if(n < 0){
         return [];
     }
-    const commentList = [];
+    const productList = [];
     Array.from(new Array(n)).forEach((eliment, index) => {
-        const comment = {
+        const product = {
             id: index,
-            name: casual.name,
-            avatar: faker.image.avatar(),
-            like: Math.floor(Math.random() * 100),
-            unchat: Math.floor(Math.random() * 10),
-            unlike: Math.floor(Math.random() * 100),
-            comment: casual.description,
-            productId: Math.floor(Math.random() * 90),
-            createAt: Date.now(),
-            updateAt: Date.now()
+            name: casual.description,
+            price: Math.floor(Math.random() * 1000),
+            rating: Math.floor(Math.random() * 5),
+            imgOne: faker.image.url(),
+            imgTwo: faker.image.url(),
+            imgThree: faker.image.url(),
+            text: casual.text
         }
-        commentList.push(comment);
+        productList.push(product);
     })
-    return commentList;
+    return productList;
 }
 
 (() => {
     //Total 100
-    const commentList = randomcommentList(1000);
+    const productList = randomproductList(501);
     // prepare db object
     const backup = {
-        comment: commentList
+        product: productList
     };
     // write db object to db.json
     fs.writeFile("backup.json", JSON.stringify(backup), () => {
